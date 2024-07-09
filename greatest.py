9bcaf215508e90827ab581293d7a252e01cdd34b
Sanika500 <sanikasanthosh500@gmail.comgit config --global user.email git config --global user.email sanikasanthosh500@gmail.comgit config --global user.email sanikasanthosh500@gmail.com>
2024-07-09 08:56:39 +0530

    done

---
+++ greatest.py
@@ -0,0 +1,36 @@
+# a=int(input("enter a number"))
+# b=int(input("enter a number"))
+
+# if(a>b):
+#     print("the largest number is:",a)
+# elif(a<b):
+#     print("the largest number is:",b)
+# else:
+#     print("both are equal")   
+
+# a=int(input("enter your age"))
+# if(a>=18):
+#     print("you are eligible to vote")
+# else:
+#     print("you are not eligible to vote")
+
+# a=int(input("enter a year"))
+# if(a%4==0):
+#     print("it is a leap year")
+# else:
+#     print("it is not a leap year")
+
+# a=int(input("enter a number"))
+# if(a>0):
+#     print("the number is positive")
+# elif(a<0):
+#     print("the number is negative")   
+# else:
+#     print("the number is zero")
+
+# a=int(input("enter a number"))
+# if(a%2==0):
+#     print("the nummber is even")     
+# else:
+#     print("the number is odd")    
+    

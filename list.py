30ede892a993cdb7ea46298682adbd58071b464b
Sanika500 <sanikasanthosh500@gmail.comgit config --global user.email git config --global user.email sanikasanthosh500@gmail.comgit config --global user.email sanikasanthosh500@gmail.com>
2024-07-12 08:59:17 +0530

    done

---
+++ list.py
@@ -0,0 +1,14 @@
+a=int(input("enter number of elements"))
+print("enter the elements")
+b=[]
+for i in range (a):
+    c=int(input())
+    b.append(c)
+    
+target=int(input("enter the target")) 
+while()
+
+
+ 
+  
+

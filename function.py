61b2e3e19411e499d7cc1748fec571ac199d7693
Sanika500 <sanikasanthosh500@gmail.comgit config --global user.email git config --global user.email sanikasanthosh500@gmail.comgit config --global user.email sanikasanthosh500@gmail.com>
2024-07-19 08:54:02 +0530

    done

---
+++ function.py
@@ -0,0 +1,19 @@
+# def add(x,y):
+#     print(x+y)
+
+# a=int(input("enter a number"))
+# b=int(input("enter a number"))
+# add(a,b)
+
+# def master(x,y):
+#     return (x+y)
+
+
+# a=int(input("enter a number"))
+# b=int(input("enter a number"))
+# print(master(a,b))
+
+ def master(x):
+        
+
+      

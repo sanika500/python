d900f84494b1d3e75a9deedb90d9bacaf02b25c1
Sanika500 <sanikasanthosh500@gmail.comgit config --global user.email git config --global user.email sanikasanthosh500@gmail.comgit config --global user.email sanikasanthosh500@gmail.com>
2024-07-27 08:53:28 +0530

    done

--- constructorr.py
+++ constructorr.py
@@ -23,7 +23,46 @@
 # p=School()
 # p.school()
 
-class Main():
-    def __init__(self,name):
-        print("my name is",name)
-p=Main("john")
+# class Main():
+#     def __init__(self,name):
+#         print("my name is",name)
+# p=Main("john")
+
+# class Main():
+#     def __init__(self,name):
+#         self.name=name
+# p=Main("john") 
+# print(p.name)   
+
+# class Master():
+#     def __init__(self,name,age):
+#         self.nam=name
+#         self.ag=age
+# p=Master("john","20")
+# print(p.nam)
+# print(p.ag)
+
+
+# class Master():
+#     def __init__(self,name,age):
+#         self.nam=name
+#         self.ag=age
+       
+#     def __str__(self):
+#            return"john  20"  
+           
+# p=Master("john","20")
+# print(p)
+
+   
+   
+class master():
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def __str__(self):
+        return f"{self.name} {self.age}"
+p1=master("john","20")
+print(p1)
+           
+        
---
+++ inheritancee.py
@@ -0,0 +1,13 @@
+class A:
+    def mainfun(self):
+        print("hello")
+        
+class B(A):
+    def mainfun2(self):
+        print("world")
+p1=A()      
+p2=B()
+p1.mainfun()
+p2.mainfun2()
+p2.mainfun()
+        

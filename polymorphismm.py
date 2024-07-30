d229956ffbefe2b5f5e6691b207331e87dbfe928
Sanika500 <sanikasanthosh500@gmail.comgit config --global user.email git config --global user.email sanikasanthosh500@gmail.comgit config --global user.email sanikasanthosh500@gmail.com>
2024-07-30 08:55:50 +0530

    done

---
+++ polymorphismm.py
@@ -0,0 +1,58 @@
+# class car:
+#     def main(self):
+#         print("move")
+        
+# class bike(car):
+#     def main(self):
+#         print("ride")
+        
+# p1=car()
+# p2=bike()
+# p1.main()
+# p2.main()
+
+
+# a=5
+# a=6
+# print(a)
+
+# class A:
+#     def main(self):
+#         print("CAS")
+# class B(A):
+#     def main(self):
+#         print("MVK")
+# class C(B):
+#     def main(self):
+#         print("IHRD")
+# p1=A()
+# p2=B()
+# p3=C()
+# p1.main()
+# p2.main()
+# p3.main()
+
+
+
+
+class School:
+    def main(self):
+        print("st mary's girls school")
+    def main(self):
+        print("2000")
+class Lp(School):
+    def main(self):
+        print("1 to 4")
+class up(school):
+    def main(self):
+        print("5 to 7")
+class highschool("School"):
+    def main(self):
+        print("8 to 10")
+class highersecondary("School"):
+    def main(self):
+        print("plus one,plus two")
+p1=(School)
+
+
+            
